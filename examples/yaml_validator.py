472ec22eee8f7bb87b0791582684949f81be6b25
Denys Bedilov <denbedilov@gmail.com>
2024-05-20 14:02:57 +0300

    yaml validator changed to function

--- examples/yaml_validator.py
+++ examples/yaml_validator.py
@@ -1,12 +1,20 @@
 from jsonschema import validate, ValidationError
 import yaml
 
-schema = yaml.safe_load(open("yaml_validator/schema.yaml"))
+_schema_path = "yaml_validator/schema.yaml"
+_yaml_path = "yaml_validator/sample.yaml"
 
-configuration = yaml.safe_load(open("yaml_validator/sample.yaml"))
+def validate_yaml(schema_path: str, yaml_path: str):
+    '''
+    validate a yaml file with given schema and returns a dict
+    '''
+    schema = yaml.safe_load(open(schema_path))
+    yaml_file = yaml.safe_load(open(yaml_path))
 
-try:
-    validate(configuration, schema)
-    print("Validation is ok")
-except ValidationError as ve:
-    raise ve
+    try:
+        validate(yaml_file, schema)
+        return yaml_file
+    except ValidationError as ve:
+        raise ve
+
+print(validate_yaml(_schema_path, _yaml_path))
